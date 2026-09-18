6e844b431df8616544145762687019fa06b40e28
fat4-alt <346881691@qq.com>
2026-09-18 16:13:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,4 +5,4 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/FZSWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/WMYWrt/g' package/base-files/files/bin/config_generate
